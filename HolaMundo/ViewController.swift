ec891bba06fc97354484633d6ca8d67ba868cfbc
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-20 21:25:57 +0200

    Better code

--- HolaMundo/ViewController.swift
+++ HolaMundo/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var lblSaludo: UILabel!
-    
     @IBOutlet var lblContador: UILabel!
     
     override func viewDidLoad() {
