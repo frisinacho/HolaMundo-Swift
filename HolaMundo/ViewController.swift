43232ddb8168f5fe8cfad8de150ce82fbe7d73ae
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-20 21:24:27 +0200

    Add lblContador connection

--- HolaMundo/ViewController.swift
+++ HolaMundo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet var lblSaludo: UILabel!
     
+    @IBOutlet var lblContador: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
