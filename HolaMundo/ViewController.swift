deaea470b752c3fbf090346f8108ebf33b6021a6
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-20 21:19:44 +0200

    Add action to TextField

--- HolaMundo/ViewController.swift
+++ HolaMundo/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ContadorCaracteres(sender: UITextField) {
+    }
 
 }
 
